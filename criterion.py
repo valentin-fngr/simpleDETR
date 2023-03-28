fixed label loss computation

criterion.py
@@ -48,7 +48,8 @@ class SetCriterion(nn.Module):
         idx = self._get_src_permutation_idx(indices) # ([number_of_objects], [number_of_objects])
         # get all label from the batch
         target_classes_o = torch.cat([t["labels"][J.type(torch.int32)] for t, (_, J) in zip(targets, indices)])
-        target_classes = torch.full(src_logits.shape[:2], self.num_classes + 1,
+        # create target classes with only no object classes, with same shape as the predicted labels 
+        target_classes = torch.full((int(src_logits.shape[0]), int(src_logits.shape[1])), self.num_classes,
                                     dtype=torch.int64, device=src_logits.device) # (bs, #Q)
         target_classes = target_classes.type(torch.float32)
         target_classes[idx] = target_classes_o
@@ -81,7 +82,7 @@ class SetCriterion(nn.Module):
         pred_boxes = outputs["boxes"] # (bs, #Q, 4)
 
         # retrieve ground truth boxes 
-        idx = self._get_src_permutation_idx(self, indices) # (batch_size*num_obj), (batch_size*num_obj)
+        idx = self._get_src_permutation_idx(indices) # (batch_size*num_obj), (batch_size*num_obj)
         target_boxes = torch.cat([t["boxes"][j] for t, (_,j) in zip(targets, indices)])
         pred_boxes = pred_boxes[idx]
 

hungarian_matcher.py
@@ -63,7 +63,7 @@ class HungarianMatcher(nn.Module):
         # compute total score
         match_cost = prob_score + distance_score + IOU_score 
         # reshape for convenience 
-        match_cost = match_cost.view(bs, num_queries, -1).cpu() # ((bs, num_queries, bs * num_total_objs)) 
+        match_cost = match_cost.view(bs, num_queries, -1).cpu() # (bs, num_queries, bs * num_total_objs)
 
         # get the information regarding how many obejcts are present for each image
         # eg : [1, 4, 10, ...] means that the first image has 1 object, the second 4, the third 10, ... 

test_all.py
@@ -41,18 +41,21 @@ class Tester(unittest.TestCase):
         matcher = matcher = HungarianMatcher(1, 1)
         self.criterion = SetCriterion(matcher, self.num_classes)
 
-    def test_can_compute_label_loss(self): 
+    @torch.no_grad()
+    def test_can_compute_loss(self): 
 
         # input image 
         img = torch.rand(*self.input_shape, device=self.device, dtype=torch.float32)
         y_true = [{"labels": torch.tensor([1, 3, 2], device=self.device, dtype=torch.float32), "boxes": torch.tensor([[10, 10, 5, 5], [10, 10, 5, 5], [10, 10, 5, 5]], device=self.device, dtype=torch.float32)} for i in range(self.batch_size)]
         output = self.model(img)
-        print(output["labels"])
         # self.assertTrue(type(output) == dict) 
         # self.assertEqual(tuple(output["labels"].shape), (self.batch_size, self.num_queries, self.num_classes + 1)) 
         # self.assertEqual(tuple(output["boxes"].shape), (self.batch_size, self.num_queries, 4))
         loss = self.criterion(y_true, output)
-
+        self.assertTrue(type(loss), dict) 
+        self.assertTrue("label_loss" in loss and "boxes_loss" in loss) 
+        self.assertTrue(len(loss["label_loss"].shape) == 0)
+        self.assertTrue(len(loss["boxes_loss"].shape) == 0)
 
 
 if __name__ == '__main__':